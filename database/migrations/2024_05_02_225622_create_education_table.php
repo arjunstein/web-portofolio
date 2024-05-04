26acaf680764c362ac59b49273cde1ba7ec194d4
arjunstein <arjun.gnw09@gmail.com>
2024-05-04 23:12:01 +0700

    ✨ feat: crud educational

---
+++ database/migrations/2024_05_02_225622_create_education_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('school');
+            $table->string('major');
+            $table->string('title');
+            $table->float('gpa');
+            $table->double('start_year');
+            $table->double('end_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('education');
+    }
+};
